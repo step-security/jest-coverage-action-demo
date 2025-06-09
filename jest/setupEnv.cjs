4256579427282d67f518ec8787b5831071554fd3
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-06-09 15:42:33 +0530

    dbj

--- jest/setupEnv.cjs
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-process.env.GITHUB_WORKSPACE = path.join(
-  path.resolve(__dirname, '..'), '.'
-)
-process.env.GITHUB_ACTION_PATH = path.join(
-  path.resolve(__dirname, '..'), '.'
-)
