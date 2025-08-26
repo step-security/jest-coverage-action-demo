3d0cb63b79b9e2f7b14b4a6ee03d56dcf6fdc0d0
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-08-26 12:47:08 +0530

    node_version specified

---
+++ src/formatMessage.js
@@ -0,0 +1,9 @@
+export default function formatMessage(message, values) {
+  let formatted = message;
+  if (values) {
+    Object.entries(values).forEach(([key, value]) => {
+      formatted = formatted.replace(new RegExp(`{${key}}`, 'g'), value);
+    });
+  }
+  return formatted;
+}
